6d30a521c0df96f7d81c5ec4897cf132eb748705
phungxuanduc2002@gmail.com <phungxuanduc2002@gmail.com>
2023-10-05 13:41:44 +0700

    array-Fruit

---
+++ NewApp/Models/Fruit.cs
@@ -0,0 +1,22 @@
+public class Fruit {
+        public int weight {get; set;}
+
+
+        // phuong thuc co gia tri tra ve-Fruit
+
+       public void EnterData()
+       {
+        System.Console.Write("Fruit name = ");
+        System.Console.Write("Fruitname = ");
+        FruitName = Console.ReadLine();
+        System.Console.Write("weight = ");
+
+ public void EnterData()
+        }
+
+    }
+    public void Display()
+    public void DisplayData()
+    {
+        System.Console.WriteLine("{0} + {1} " , FruitName, weight);
+    }
